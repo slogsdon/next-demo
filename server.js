e4c8a64c6ce125e49f899d1b69427e910758f842
Shane Logsdon <shane@shanelogsdon.com>
2017-03-29 23:38:19 -0400

    listen on  if set

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.prepare()
     server.get('/blog/:slug', (req, res) => renderAndCache(req, res, '/blog', req.params));
     server.get('*', (req, res) => handler(req, res));
 
-    server.listen(3000, (err) => {
+    server.listen(process.env.PORT || 3000, (err) => {
         if (err) throw err;
         console.log('> Ready on http://localhost:3000');
       });
